79f824508ffec762680f996d1e5c69bd0de58338
sabbirshawon <sabbirs93@gmail.com>
2021-10-04 22:30:56 +0600

    db config file created

---
+++ api/db_config.php
@@ -0,0 +1,11 @@
+<?php
+
+    $hostName = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "db_crud";
+
+    $mysqli = new mysqli($hostName, $username, $password, $dbname);
+
+
+?>
