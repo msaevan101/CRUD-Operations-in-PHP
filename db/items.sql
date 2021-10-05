8190706e114505154b81ef2c2b3a6eff88a81fe0
A.T.M Shamiul Bashir <55659852+01shamiul@users.noreply.github.com>
2021-10-05 23:42:58 +0600

    Update items.sql

--- db/items.sql
+++ db/items.sql
@@ -4,6 +4,8 @@ CREATE DATABASE db_crud;
 CREATE TABLE IF NOT EXISTS `tbl_items` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `description` text COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=63 ;
