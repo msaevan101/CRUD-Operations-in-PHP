fd2db9e845411545f8e42d3029e6d7a52345b576
tanjinamumu <tanjinamumu@gmail.com>
2021-10-23 20:56:24 +0600

    index,footer,header files updated

--- index.php
+++ index.php
@@ -1,25 +1,12 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>PHP Jquery Ajax CRUD Example</title>
-    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twbs-pagination/1.3.1/jquery.twbsPagination.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/1000hz-bootstrap-validator/0.11.5/validator.min.js"></script>
-    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
-    <link href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css" rel="stylesheet">
-    <script type="text/javascript">
-        var url = "http://localhost:8000/";
-    </script>
-    <script src="/js/item-ajax.js"></script>
-</head>
+<?php 
+include 'header.php';
+?>
 <body>
-    <div class="container">
+    <div class="container crud-container">
         <div class="row">
             <div class="col-lg-12 margin-tb">
                 <div class="pull-left">
-                  <h2>PHP Jquery Ajax CRUD Example</h2>
+                  <h2>PHP CRUD Operation</h2>
                 </div>
                 <div class="pull-right">
                 <button type="button" class="btn btn-success" data-toggle="modal" data-target="#create-item">
@@ -108,5 +95,7 @@
           </div>
         </div>
     </div>
-</body>
-</html>
+
+<?php 
+include 'footer.php';
+?>
